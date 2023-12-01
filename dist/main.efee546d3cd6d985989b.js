1b5d38ae7b12e9dc50bf90f4fbbc31e1b9d3cbb5
Сергей Токмаков <s.tokmakov@ermolinskie.com>
2023-12-01 13:26:54 +0300

    linter

---
+++ dist/main.efee546d3cd6d985989b.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ (() => {
+
+eval("\r\nfetch('http://localhost:3000/people')\r\n  .then((response) => {\r\n    return response.json();\r\n  })\r\n  .then((data) => {\r\n    document.write('Hello, webpack!!'+\"<BR>\"+\"<BR>\"+\"DATA JSON:  \"+JSON.stringify(data));\r\n  })\r\n  .catch((error) => {\r\n    console.error('Ошибка загрузки JSON:', error);\r\n  }); \r\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./index.js"]();
+/******/ 	
+/******/ })()
+;
--- dist/main.js
+++
@@ -1,1 +0,0 @@
-fetch("http://localhost:3000/people").then((e=>e.json())).then((e=>{document.write("Hello, webpack!!<BR><BR>DATA JSON:  "+JSON.stringify(e))})).catch((e=>{console.error("Ошибка загрузки JSON:",e)}));
